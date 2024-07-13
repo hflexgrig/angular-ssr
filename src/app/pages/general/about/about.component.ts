9b2822cf0c7054a85d0a0c83a8d29070cbb71517
hflex <haykflexgrigoryan@gmail.com>
2024-07-13 21:44:09 +0400

    added about c

--- src/app/pages/general/about/about.component.ts
+++ src/app/pages/general/about/about.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  private titleService = inject(Title);
 
+  ngOnInit(): void {
+    this.titleService.setTitle("about page");
+  }
 }
